76bb7075fbf3a5db3568305a6a3640e76f38bda0
최영재 <brandon94_@naver.com>
2020-10-08 04:43:28 +0900

    백준 4344

---
+++ Baekjoon 4344/baekjoon 4344.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+// #include <cmath>
+using namespace std;
+
+int main()
+{
+    int C, N;
+    int grade[1000];
+    float sum = 0;
+    float count = 0;
+    float rate;
+
+    cin >> C;
+    for (int i = 0; i < C; i++)
+    {
+        cin >> N;
+        count = sum = 0;
+        for (int q = 0; q < N; q++)
+        {
+            cin >> grade[q];
+            sum += grade[q];
+        }
+        for (int q = 0; q < N; q++)
+        {
+            if (grade[q] > sum / N)
+            {
+                count++;
+            }
+        }
+        cout << fixed;
+        cout.precision(3);
+        cout << (count / N) * 100 << "%" << endl;
+    }
+}
