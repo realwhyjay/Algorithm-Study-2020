c90f5f887390be2ad267e13a729a0c16bdbafb4e
최영재 <brandon94_@naver.com>
2020-10-20 16:35:34 +0900

    백준 3085 풀이

---
+++ 3Week - Baekjoon 3085/baekjoon3085.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+using namespace std;
+
+int n;
+int max_count = 0;
+char candy[51][51];
+char temp;
+void vertical();
+void horizontal();
+
+int main()
+{
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int q = 0; q < n; q++)
+        {
+            cin >> candy[i][q];
+        }
+    }
+
+    //세로 변경
+    for (int i = 0; i < n; i++)
+    {
+        for (int q = 0; q < n - 1; q++)
+        {
+            temp = candy[i][q];
+            candy[i][q] = candy[i][q + 1];
+            candy[i][q + 1] = temp;
+
+            vertical();
+            horizontal();
+
+            temp = candy[i][q + 1];
+            candy[i][q + 1] = candy[i][q];
+            candy[i][q] = temp;
+        }
+    }
+    //가로 변경
+    for (int q = 0; q < n; q++)
+    {
+        for (int i = 0; i < n - 1; i++)
+        {
+            temp = candy[i][q];
+            candy[i][q] = candy[i + 1][q];
+            candy[i + 1][q] = temp;
+
+            vertical();
+            horizontal();
+
+            temp = candy[i + 1][q];
+            candy[i + 1][q] = candy[i][q];
+            candy[i][q] = temp;
+        }
+    }
+
+    cout << max_count << endl;
+}
+
+// vertical for문 조건과 horizontal for문 조건 제대로 확인해야함.
+void vertical()
+{
+    for (int q = 0; q < n; q++)
+    {
+        int count = 1;
+        for (int i = 0; i < n; i++)
+        {
+            if (candy[i][q] == candy[i + 1][q])
+            {
+                count++;
+            }
+            else
+            {
+                if (max_count < count)
+                {
+                    max_count = count;
+                }
+                count = 1;
+            }
+        }
+    }
+}
+
+void horizontal()
+{
+    for (int i = 0; i < n; i++)
+    {
+        int count = 1;
+        for (int q = 0; q < n; q++)
+        {
+            if (candy[i][q] == candy[i][q + 1])
+            {
+                count++;
+            }
+            else
+            {
+                if (max_count < count)
+                {
+                    max_count = count;
+                }
+                count = 1;
+            }
+        }
+    }
+}
